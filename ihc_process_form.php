75cba8ad47afbd31548b0d644ad954f5561f4a3e
vahe-hass <vhassratian@gmail.com>
2023-11-05 20:42:37 +0330

    making new form

--- ihc_process_form.php
+++ ihc_process_form.php
@@ -1,16 +1,20 @@
 <?php
+
 // Include WordPress core functions
 require_once("../../../wp-load.php");
+global $wpdb;
 
 // Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    global $wpdb;
     $table_name = $wpdb->prefix . 'ip_hero_changer';
+    $primary_key = 'id';
 
     $user_country = sanitize_text_field($_POST["country"]);
     $user_state = sanitize_text_field($_POST["state"]);
     $user_city = sanitize_text_field($_POST["city"]);
-    $user_option = sanitize_text_field($_POST["option"]);
+    // $user_option = sanitize_text_field($_POST["option"]);
+    $user_option = "B";
+    $user_color = sanitize_text_field($_POST["color"]);
 
     $wpdb->insert(
         $table_name,
@@ -19,6 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             'user_state' => $user_state,
             'user_city' => $user_city,
             'user_option' => $user_option,
+            'user_color' => $user_color,
             'submission_date' => current_time('mysql', 1)
         )
     );
@@ -27,10 +32,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo "An error occurred while saving the form data.";
     } else {
         echo "Form data saved successfully.";
+
+        $wpdb->insert(
+            $table_name,
+            array(
+                'user_country' => "user country A",
+                'user_state' => "user state A",
+                'user_city' => "user city A",
+                'user_option' => "A",
+                'user_color' => "color A",
+                'submission_date' => current_time('mysql', 1)
+            )
+        );
     }
 } else {
     // Invalid request method
     http_response_code(405);
     echo "Invalid request method.";
 }
+
 ?>
